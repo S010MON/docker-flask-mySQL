9444d6a1b5c2b4e276eb910a7fbf1e199a79ff1c
S010MON <leon@bdebnath.com>
2021-09-24 13:17:59 +0200

    add host=0.0.0.0

--- app/PizzaService.py
+++ app/PizzaService.py
@@ -56,5 +56,5 @@ api.add_resource(Customer, '/customer')
 api.add_resource(Order, '/order')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=5000)
+    app.run(debug=True, host='0.0.0.0' ,port=5000)
 
