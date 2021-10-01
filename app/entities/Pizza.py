b1b609494e6db12b6769ae43445d15963b9e1473
S010MON <leon@bdebnath.com>
2021-10-01 11:24:25 +0200

    Veg added to const

--- app/entities/Pizza.py
+++ app/entities/Pizza.py
@@ -1,12 +1,12 @@
 class Pizza:
 
-    def __init__(self, pizza_id, name, cost_euro, cost_cents, toppings):
+    def __init__(self, pizza_id, name, cost_euro, cost_cents, toppings, vegetarian):
         self.pizza_id = pizza_id
         self.name = name
         self.cost_euro = cost_euro
         self.cost_cents = cost_cents
         self.toppings = toppings
-        self.vegetarian = True
+        self.vegetarian = vegetarian
 
     def to_dict(self):
         return {"pizza_id": self.pizza_id,
