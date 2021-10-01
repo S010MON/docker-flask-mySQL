3d31aa73f7ab81e10458250e85e3daef42ddea13
S010MON <leon@bdebnath.com>
2021-10-01 21:14:42 +0200

    Refactored Service<->Controller connections

--- app/PizzaController.py
+++ app/PizzaController.py
@@ -1,105 +1,135 @@
 from flask import jsonify
 from datetime import datetime, timedelta
-import json
 import PizzaPersistence as db
 
 '''
 Pizza Controller
-- Converts objects into JSON serialisable data
-- Checks for logical errors and returns None if unable to complete query
+- Query Logic
+- Error Management
+- JSON serialisation
 '''
+
 
 def get_all_pizzas():
     pizzas = db.get_all_pizzas()
-    if pizzas == None:
+    if pizzas is None:
         return not_found_404()
 
     data = []
     for i in range(0, len(pizzas)):
         data.append(pizzas[i].to_dict())
-    
-    return jsonify( message="test message",
-                    category="success",
-                    data=data,
-                    status=200)
+
+    return jsonify(message="pizzas",
+                   category="success",
+                   data=data,
+                   status=200)
+
 
 def get_all_drinks():
     drinks = db.get_all_drinks()
+    if drinks is None:
+        return not_found_404()
+
     data = []
     for i in range(0, len(drinks)):
         data.append(drinks[i].to_dict())
-    return data
+
+    return jsonify(message="drinks",
+                   category="success",
+                   data=data,
+                   status=200)
+
 
 def get_all_desserts():
     desserts = db.get_all_desserts()
+    if desserts is None:
+        return not_found_404()
+
     data = []
     for i in range(0, len(desserts)):
         data.append(desserts[i].to_dict())
-    return data
+
+    return jsonify(message="Sweet Sweets!",
+                   category="success",
+                   data=data,
+                   status=200)
+
 
 def get_customer_by_id(customer_id):
     data = db.get_customer(customer_id).to_dict()
     return data
 
+
 def get_delivery_driver_by_id(driver_id):
     return None
 
+
 def get_purchase_by_id(purchase_id):
     data = db.get_purchase(purchase_id).to_dict()
-    if data == None:
+    if data is None:
         return not_found_404()
     else:
-        return jsonify( message="purchase",
-                        category="success",
-                        data=data,
-                        status=200)
+        return jsonify(message="purchase",
+                       category="success",
+                       data=data,
+                       status=200)
+
 
 def post_customer(customer):
     if db.customer_exists(customer):
-        return None
+        return jsonify(message="customer already exists",
+                       category="success",
+                       status=201)
     else:
         customer.address = db.create_address(customer.address)
         data = db.create_customer(customer).to_dict()
-    return customer.to_dict()
+        return jsonify(message="customer added",
+                       category="success",
+                       data=data,
+                       status=201)
+
 
 def post_purchase(purchase):
-    if purchase.pizzas == None:
-        return jsonify( message="order must contain a pizza",
-                        category="failed",
-                        data=None,
-                        status=400)
-    
-    data =  db.create_purchase(purchase).to_dict() 
-    return jsonify( message="pizzas",
-                    category="success",
-                    data=data,
-                    status=200)
+    if purchase.pizzas is None:
+        return jsonify(message="order must contain a pizza",
+                       category="failed",
+                       data=None,
+                       status=400)
+
+    data = db.create_purchase(purchase).to_dict()
+    return jsonify(message="pizzas",
+                   category="success",
+                   data=data,
+                   status=200)
+
 
 def delete_purchase(purchase_id):
     purchase = db.get_purchase(purchase_id)
-    if purchase == None:
-        return jsonify( message="Order does not exist",
-                        category="failed",
-                        data=None,
-                        status=404)
-    
+    if purchase is None:
+        return jsonify(message="Order does not exist",
+                       category="failed",
+                       data=None,
+                       status=404)
+
     last_minute = purchase.datetime + timedelta(minutes=5)
     if datetime.now() > last_minute:
-        return jsonify( message="Cannot Cancel Order",
-                        category="failed",
-                        data=purchase.to_dict(),
-                        status=400)
-    data = db.delete_purchase(purchase_id).to_dict()
-    return jsonify( message="Order Deleted",
-                    category="success",
-                    data=purchase.to_dict(),
-                    status=200)
+        return jsonify(message="Cannot Cancel Order",
+                       category="failed",
+                       data=purchase.to_dict(),
+                       status=400)
+
+    else:
+        return jsonify(message="Order Deleted",
+                       category="success",
+                       data=purchase.to_dict(),
+                       status=200)
+
 
 def not_found_404():
-    return jsonify( message="Not found",
-                    status=404)
+    return jsonify(message="Not found",
+                   status=404)
+
 
 def not_implemented_501():
-    return jsonify( message='Not yet implemented',
-                    status=501)
-
+    return jsonify(message='Not yet implemented',
+                   status=501)
--- app/PizzaService.py
+++ app/PizzaService.py
@@ -1,12 +1,10 @@
-import os
-import json
 from flask_cors import CORS
-import PizzaController as controller
-from entities.Customer import Customer as Customer_object
-from entities.Purchase import Purchase as Purchase_object
-from entities.Address import Address
-from flask import Flask, jsonify, request
+from flask import Flask
 from flask_restful import Resource, Api, reqparse
+import PizzaController as ctrlr
+from entities.Customer import Customer as Customer_obj
+from entities.Purchase import Purchase as Purchase_obj
+from entities.Address import Address
 
 app = Flask(__name__)
 api = Api(app)
@@ -15,46 +13,36 @@ CORS(app)
 '''
 Pizza Service
 - API endpoints
-- 
+- JSON Input Parsing
 '''
+
 
 class Home(Resource):
     def get(self):
         return {"message":"Welcome to Pizza Maastricht"}, 200
 
+
 class Pizza(Resource):
     def get(self):
-        response = controller.get_all_pizzas()
-        return response
+        return ctrlr.get_all_pizzas()
+
 
 class Drink(Resource):
     def get(self):
-        data = controller.get_all_drinks()
-        if data == None:
-            return not_found_404()
-        else:
-            return jsonify( message="test message",
-                            category="success",
-                            data=data,
-                            status=200)
+        return ctrlr.get_all_drinks()
+
 
 class Dessert(Resource):
     def get(self):
-        data = controller.get_all_desserts()
-        if data == None:
-            return not_found_404()
-        else:
-            return jsonify( message="Sweet Sweets!",
-                            category="success",
-                            data=data,
-                            status=200)
+        return ctrlr.get_all_desserts()
+
 
 class Customer(Resource):
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('customer_id', type=int)
         args = parser.parse_args()
-        return controller.get_customer_by_id(args['customer_id']),200
+        return ctrlr.get_customer_by_id(args['customer_id']), 200
 
     def post(self):
         parser = reqparse.RequestParser()
@@ -65,34 +53,18 @@ class Customer(Resource):
         parser.add_argument('phone_number', type=str)
         args = parser.parse_args()
 
-        address = Address(args['street'],
-                          args['town'],
-                          args['postcode'])
-        
+        address = Address(args['street'], args['town'], args['postcode'])
+        customer = Customer_obj(args['customer_name'], address, args['phone_number'])
+        return ctrlr.post_customer(customer)
 
-        customer = Customer_object(args['customer_name'],
-                                   address,
-                                   args['phone_number'])
-        data = controller.post_customer(customer)
-        if data == None:
-            return jsonify( message="customer already exists",
-                            category="success",
-                            data=data,
-                            status=201)
-
-
-        else:
-            return jsonify( message="customer added",
-                            category="success",
-                            data=data,
-                            status=201)
 
 class Purchase(Resource):
     def get(self):
         parser = reqparse.RequestParser()
         parser.add_argument('purchase_id', type=int)
         args = parser.parse_args()
-        response = controller.get_purchase_by_id(args['purchase_id'])
+
+        response = ctrlr.get_purchase_by_id(args['purchase_id'])
         return response
 
     def post(self):
@@ -103,24 +75,18 @@ class Purchase(Resource):
         parser.add_argument('desserts', type=dict, action='append')
         args = parser.parse_args()
         
-        purchase = Purchase_object(args['customer_id'], args['pizzas'], args['drinks'], args['desserts'])
-        response = controller.post_purchase(purchase) 
+        purchase = Purchase_obj(args['customer_id'], args['pizzas'], args['drinks'], args['desserts'])
+        response = ctrlr.post_purchase(purchase)
         return response
 
     def delete(self):
         parser = reqparse.RequestParser()
         parser.add_argument('purchase_id', type=int)
         args = parser.parse_args()
-        response = controller.delete_purchase(args['purchase_id'])
+
+        response = ctrlr.delete_purchase(args['purchase_id'])
         return response
 
-def not_found_404():
-    return jsonify( message="Not found",
-                    status=404)
-
-def not_implemented_501():
-    return jsonify( message='Not yet implemented',
-                    status=501)
 
 api.add_resource(Home, '/')
 api.add_resource(Pizza, '/pizza')
@@ -130,5 +96,5 @@ api.add_resource(Customer, '/customer')
 api.add_resource(Purchase, '/purchase')
 
 if __name__ == '__main__':
-    app.run(debug=True, host='0.0.0.0' ,port=5000)
+    app.run(debug=True, host='0.0.0.0', port=5000)
 
