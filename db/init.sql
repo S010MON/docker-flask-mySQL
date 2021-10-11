855ff24343200eda240a62de9bfdc85d95fecd1e
k-san-official <konstantin.sandfort@gmail.com>
2021-10-11 15:01:40 +0200

    finishing up

--- db/init.sql
+++ db/init.sql
@@ -30,58 +30,22 @@ CREATE TABLE Address (
     street VARCHAR(255) NOT NULL,
     town VARCHAR(255) NOT NULL,
     postcode VARCHAR(6) NOT NULL,
-    PRIMARY KEY (address_id));
+    primary key (address_id));
 
-CREATE TABLE Pizza (
-    pizza_id INT NOT NULL AUTO_INCREMENT,
-    pizza_name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (pizza_id));
+CREATE TABLE Customer (
+    customer_id INT NOT NULL AUTO_INCREMENT,
+    name VARCHAR(255),
+    address_id INT NOT NULL,
+    phone_number VARCHAR(255) NOT NULL,
+    primary key (customer_id),
+    CONSTRAINT FK_address_id_c FOREIGN KEY (address_id) REFERENCES Address(address_id));
 
-CREATE TABLE Drink (
-    drink_id INT NOT NULL AUTO_INCREMENT,
-    drink_name VARCHAR(255) NOT NULL,
-    drink_price DOUBLE NOT NULL,
-    PRIMARY KEY (drink_id));
-
-CREATE TABLE Dessert (
-    dessert_id INT NOT NULL AUTO_INCREMENT,
-    dessert_name VARCHAR(255) NOT NULL,
-    dessert_price DOUBLE NOT NULL,
-    PRIMARY KEY (dessert_id));
-
-CREATE TABLE ToppingMapping (
-    topping_map_id INT NOT NULL AUTO_INCREMENT,
-    pizza_id INT NOT NULL,
-    topping_id INT NOT NULL,
-    PRIMARY KEY (topping_map_id));
-
-CREATE TABLE PizzaMapping (
-    pizza_map_id INT NOT NULL AUTO_INCREMENT,
-    purchase_id INT NOT NULL,
-    pizza_id INT NOT NULL,
-    quantity INT NOT NULL,
-    PRIMARY KEY (pizza_map_id));
-
-CREATE TABLE DrinkMapping (
-    drink_map_id INT NOT NULL AUTO_INCREMENT,
-    purchase_id INT NOT NULL,
-    drink_id INT NOT NULL,
-    quantity INT NOT NULL,
-    PRIMARY KEY (drink_map_id));
-
-CREATE TABLE DessertMapping (
-    dessert_map_id INT NOT NULL AUTO_INCREMENT,
-    purchase_id INT NOT NULL,
-    dessert_id INT NOT NULL,
-    quantity INT NOT NULL,
-    PRIMARY KEY (dessert_map_id));
-
-CREATE TABLE Topping (
-    topping_id INT NOT NULL AUTO_INCREMENT,
-    topping_name VARCHAR(255) NOT NULL,
-    vegetarian BOOL NOT NULL,
-    topping_price DOUBLE NOT NULL,
-    PRIMARY KEY (topping_id));
+CREATE TABLE DeliveryDriver (
+    driver_id INT NOT NULL AUTO_INCREMENT,
+    operating_area VARCHAR(6) NOT NULL,
+    on_task BOOL NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    primary key (driver_id));
 
 CREATE TABLE Purchase (
     purchase_id INT NOT NULL AUTO_INCREMENT,
@@ -89,28 +53,44 @@ CREATE TABLE Purchase (
     customer_id INT NOT NULL,
     delivery_driver_id INT,
     status VARCHAR(255) NOT NULL, # accepted, dispatched, delivered
-    PRIMARY KEY (purchase_id));
+    PRIMARY KEY (purchase_id),
+    CONSTRAINT FK_customer_id_pu FOREIGN KEY (customer_id) REFERENCES Customer(customer_id),
+    CONSTRAINT FK_delivery_driver_id_pu FOREIGN KEY (delivery_driver_id) REFERENCES DeliveryDriver(driver_id));
 
-CREATE TABLE Customer (
-    customer_id INT NOT NULL AUTO_INCREMENT,
-    name VARCHAR(255),
-    address_id INT NOT NULL,
-    phone_number VARCHAR(255) NOT NULL,
-    PRIMARY KEY (customer_id));
+CREATE TABLE ToppingMapping (
+    topping_map_id INT NOT NULL AUTO_INCREMENT,
+    pizza_id INT NOT NULL,
+    topping_id INT NOT NULL,
+    PRIMARY KEY (topping_map_id),
+    CONSTRAINT FK_pizza_id_to FOREIGN KEY (pizza_id) REFERENCES Pizza(pizza_id),
+    CONSTRAINT FK_topping_id_to FOREIGN KEY (topping_id) REFERENCES Topping(topping_id));
 
-CREATE TABLE DeliveryDriver (
-    driver_id INT NOT NULL AUTO_INCREMENT,
-    operating_area VARCHAR(6) NOT NULL,
-    on_task BOOL NOT NULL,
-    name VARCHAR(255) NOT NULL,
-    PRIMARY KEY (driver_id));
+CREATE TABLE PizzaMapping (
+    pizza_map_id INT NOT NULL AUTO_INCREMENT,
+    purchase_id INT NOT NULL,
+    pizza_id INT NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (pizza_map_id),
+    CONSTRAINT FK_purchase_id_pi FOREIGN KEY (purchase_id) REFERENCES Purchase(purchase_id),
+    CONSTRAINT FK_pizza_id_pi FOREIGN KEY (pizza_id) REFERENCES Pizza(pizza_id));
 
-CREATE TABLE Address (
-    address_id INT NOT NULL AUTO_INCREMENT,
-    street VARCHAR(255) NOT NULL,
-    town VARCHAR(255) NOT NULL,
-    postcode VARCHAR(6) NOT NULL,
-    PRIMARY KEY (address_id));
+CREATE TABLE DrinkMapping (
+    drink_map_id INT NOT NULL AUTO_INCREMENT,
+    purchase_id INT NOT NULL,
+    drink_id INT NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (drink_map_id),
+    CONSTRAINT FK_purchase_id_dr FOREIGN KEY (purchase_id) REFERENCES Purchase(purchase_id),
+    CONSTRAINT FK_drink_id_dr FOREIGN KEY (drink_id) REFERENCES Drink(drink_id));
+
+CREATE TABLE DessertMapping (
+    dessert_map_id INT NOT NULL AUTO_INCREMENT,
+    purchase_id INT NOT NULL,
+    dessert_id INT NOT NULL,
+    quantity INT NOT NULL,
+    PRIMARY KEY (dessert_map_id),
+    CONSTRAINT FK_purchase_id_de FOREIGN KEY (purchase_id) REFERENCES Purchase(purchase_id),
+    CONSTRAINT FK_dessert_id_de FOREIGN KEY (dessert_id) REFERENCES Dessert(dessert_id));
 
 CREATE TABLE Discount (
     discount_id INT NOT NULL AUTO_INCREMENT,
